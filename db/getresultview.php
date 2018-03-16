aa69df8c32764c39efc7f649125c296371e89860
Anuj Yadav <anujy647@gmail.com>
2018-03-16 21:44:31 +0530

    remove hint

--- db/getresultview.php
+++ db/getresultview.php
@@ -1,6 +1,7 @@
 <?php
-session_start();
+// session_start();
 require_once("connect.php");
 $query = "select * from bnoisesResult where unique_user_id='".$_SESSION['unique_user_id']."' and que_ans_seq IS NOT NULL";
+// echo $query;
 $result = mysqli_query($conn,$query);
 ?>
